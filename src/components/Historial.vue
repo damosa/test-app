8e18a5eb3586142a895c113e6a7b072fef95bcf4
damosa <dasamo1010@gmail.com>
2021-01-28 15:58:02 -0500

    Historical finished

--- src/components/Historial.vue
+++ src/components/Historial.vue
@@ -1,20 +1,57 @@
 <template>
   <div>
     <b-card-text>Historial</b-card-text>
-    <h4>Fecha consulta:</h4>
+    <h4>Fecha consulta: {{ currentDate }}</h4>
     <div>
-      <b-table></b-table>
+      <b-table striped hover :items="historical" :fields="fields"></b-table>
     </div>
   </div>
 </template>
 <script>
+import DataService from "../services/DataService";
+
 export default {
-    name: "Historial",
-    data() {
+  name: "Historial",
+  data() {
+    return {
+      currentDate: "",
+      historical: [],
+      fields: [
+        { key: "name", label: "Nombre" },
+        { key: "abbreviation", label: "Abreviatura" },
+        { key: "price", label: "Precio" },
+      ],
+    };
+  },
+  mounted() {
+    this.currentDate = this.getDate();
+    this.getHistorical();
+  },
+  methods: {
+    getDate() {
+      let date = new Date();
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      const dateFormat =
+        month < 10 ? `${year}-0${month}-${day}` : `${day}-${month}-${year}`;
+      return dateFormat;
+    },
+    async getHistorical() {
+      const { data: listNamesDivisas } = await DataService.getNamesDivisas();
+      const {
+        data: { rates: historical },
+      } = await DataService.getHistorical(this.currentDate);
+      console.log(listNamesDivisas, historical);
+      this.historical = Object.keys(historical).map((key) => {
         return {
-            
-        }
-    }
+          name: listNamesDivisas[key],
+          abbreviation: key,
+          price: historical[key],
+        };
+      });
+    },
+  },
 };
 </script>
 <style lang="stylus" scoped></style>
--- src/services/DataService.js
+++ src/services/DataService.js
@@ -2,12 +2,17 @@ import axios from "../http-common";
 
 const APP_ID = "ba2aec586c2e4bc9a56a970dd9f781c3"
 class DataService {
-  static getNamesDivisas() {
+  // static para poder acceder a los metodos de la clasec cuando se importe sin crear un new
+  static getNamesDivisas = () => {
     return axios.get(`/currencies.json?app_id=${APP_ID}`);
   }
 
-  static getListDivisas() {
+  static getListDivisas = () => {
     return axios.get(`/latest.json?app_id=${APP_ID}`);
+  }
+
+  static getHistorical = (date) => {
+    return axios.get(`/historical/${date}.json?app_id=${APP_ID}&base=USD`);
   }
 }
 
